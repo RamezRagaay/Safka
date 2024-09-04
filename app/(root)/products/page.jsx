b0d6d8761672321b8e0aa8c16ef47bc9953aba82
Ephraim Youssef <ephraim.youssf@gmail.com>
2024-09-04 04:44:25 +0300

    ProductsLIst's SideBar Done with searchParams and without API V1.0

---
+++ app/(root)/products/page.jsx
@@ -0,0 +1,12 @@
+import ProductsList from '@/components/pages/productsList/ProductsList'
+import React from 'react'
+
+const page = () => {
+	return (
+		<div>
+			<ProductsList />
+		</div>
+	)
+}
+
+export default page
---
+++ components/pages/productsList/ProductsList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideBar from './SideBar'
+
+const ProductsList = () => {
+  return (
+    <div className='flex container my-10'>
+      <SideBar />
+    </div>
+  )
+}
+
+export default ProductsList
