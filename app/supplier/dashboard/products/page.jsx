4ad66d6ef5c1652d7beff00fa944b5066d04818f
RamezRagaay <ramez5ragay@gmail.com>
2024-09-04 05:21:28 +0300

    button for supplier products

--- app/supplier/dashboard/products/page.jsx
+++ app/supplier/dashboard/products/page.jsx
@@ -1,13 +1,15 @@
+import { PlusIcon } from 'lucide-react'
 import React from 'react'
 
 const Page = () => {
   return (
     <>
-        <div>
-            <h1>
-                Products
-            </h1>
-        </div>
+        <button className='mt-12 mr-12 flex items-center h-12 gap-2 p-4 rounded-3xl bg-slate-200 text-slate-700 hover:bg-slate-300 transition-all duration-300'>
+            <PlusIcon className='w-8 h-8 rounded-full bg-primary text-slate-100'/>
+            <span className='text-2xl font-semibold'>
+                إضافة منتج
+            </span>
+        </button>
     </>
 )
 }
